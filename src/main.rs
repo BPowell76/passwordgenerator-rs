fc0b68589d18712f342dcc7b519a0eb9f7238808
Brian Powell Jr <b.powell.jr@outlook.com>
2025-08-24 11:42:21 -0400

    Change UI to use native from cosmic. Setup a basic callback wiring. Added horizontal spacing the special characters horizontal box

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use slint::private_unstable_api::re_exports::Float;
+use slint::ToSharedString;
+
 slint::include_modules!();
 fn main() {
     let main_window = MainWindow::new().unwrap();
     let main_window_weak = main_window.as_weak();
+
+    main_window.on_generate_password( move || {
+        let password_length: u8 = main_window_weak.unwrap().get_password_length() as u8;
+        let special_characters: bool = main_window_weak.unwrap().get_use_special_characters();
+
+        let mut password: &str = "";
+
+        main_window_weak.unwrap().set_password(password.to_shared_string());
+    });
 
     main_window.run();
 }
