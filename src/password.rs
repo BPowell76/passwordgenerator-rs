70a7e766ca7f16726e1e5488aea520ff75a82c68
Brian Powell Jr <brian.powell.76@proton.me>
2026-06-15 20:36:28 -0400

    Fix typos Remove artifact left behind from changing how password string is built. Removed underscores from variables that were private as it lead to silencing compiler warnings. Removed verbose &mut from places that rust was going to take care of it at compile time.

--- src/password.rs
+++ src/password.rs
@@ -2,28 +2,28 @@ use rand::prelude::*;
 
 fn build_ascii_character_vector(special_characters: bool) -> Vec<u32> {
     let mut vector: Vec<u32> = Vec::new();
-    let mut _character_code: u32 = 33;
+    let mut character_code: u32 = 33;
 
-    while _character_code < 127 {
+    while character_code < 127 {
 
-        if _character_code == 34 {
-            _character_code += 1;
+        if character_code == 34 {
+            character_code += 1;
             continue;
         }
 
         if special_characters {
-            if char::from_u32(_character_code).unwrap().is_ascii_graphic()
-                && !char::from_u32(_character_code).unwrap().is_ascii_alphanumeric() {
-                (&mut vector).push(_character_code);
+            if char::from_u32(character_code).unwrap().is_ascii_graphic()
+                && !char::from_u32(character_code).unwrap().is_ascii_alphanumeric() {
+                vector.push(character_code);
             }
         }
         else {
-            if char::from_u32(_character_code).unwrap().is_ascii_alphanumeric() {
-                (&mut vector).push(_character_code);
+            if char::from_u32(character_code).unwrap().is_ascii_alphanumeric() {
+                vector.push(character_code);
             }
         }
 
-        _character_code += 1;
+        character_code += 1;
     }
 
     return vector;
@@ -52,32 +52,30 @@ fn build_password(use_special_characters: bool, password_length: u8) -> String {
 
 fn add_special_characters(password_length: u8, password_string: String) -> String {
     let mut rng = rand::rng();
-    let mut _rand_index: u32;
-    let mut _password_vector: Vec<char> = password_string.chars().collect();
+    let mut rand_index: u32;
+    let mut password_vector: Vec<char> = password_string.chars().collect();
     let mut special_character_count: u8 = 0;
     let character_vector: Vec<u32> = build_ascii_character_vector(true);
-    let mut _character: char;
+    let mut character: char;
 
     // Set special character limit to a value between 1 half of the password's length
-    let special_character_limit: u8 = (&mut rng).random_range(1..(password_length / 2));
+    let special_character_limit: u8 = rng.random_range(1..(password_length / 2));
 
     // Replace alphanumeric characters with special characters until the limit is reached
-    if special_character_count < special_character_limit {
-        while special_character_count < special_character_limit {
-            _rand_index = (&mut rng).random_range(.._password_vector.len()) as u32;
-            _character = _password_vector[_rand_index as usize];
-            if _character.is_ascii_graphic() && !_character.is_ascii_alphanumeric() {
-                continue;
-            }
-            else {
-                let graphic_index = (&mut rng).random_range(..character_vector.len()) as u32;
-                _password_vector[_rand_index as usize] = char::from_u32(character_vector[graphic_index as usize]).unwrap();
-                special_character_count += 1;
-            }
+    while special_character_count < special_character_limit {
+        rand_index = rng.random_range(..password_vector.len()) as u32;
+        character = password_vector[rand_index as usize];
+        if character.is_ascii_graphic() && !character.is_ascii_alphanumeric() {
+            continue;
+        }
+        else {
+            let graphic_index = rng.random_range(..character_vector.len()) as u32;
+            password_vector[rand_index as usize] = char::from_u32(character_vector[graphic_index as usize]).unwrap();
+            special_character_count += 1;
         }
     }
 
-    return _password_vector.into_iter().collect();
+    return password_vector.into_iter().collect();
 }
 
 pub fn create_password(use_spec_char: bool, length: u8) -> String {
